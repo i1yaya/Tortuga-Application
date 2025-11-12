4b27067c9347bd447a7a4790963b94c59dfe5015
Ilya Ruyatkin <yhurt100@mail.ru>
2025-11-12 19:54:20 +0300

    Add files via upload

---
+++ NewProject/NewProject/NewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewProjectApp.swift
+//  NewProject
+//
+//  Created by Ilya Ruyatkin on 12.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct NewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
